be4ba81b36d080fc5244baa1d48c8eacb8382c31
whoami-3F <saurav.stamang@gmail.com>
2024-11-06 22:14:53 +0545

    feat: add Mongoose connectDB function with error handling

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () =>{
+    try{
+        const connectionInstance = await mongoose.connect
+        (`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+        console.log(`MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`)
+    }catch(error){
+        console.log("MongoDB Connection Error",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
