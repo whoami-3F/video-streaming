f1f5d49ac211cbf6e9ba4a3967571eb237b6a4f8
whoami-3F <saurav.stamang@gmail.com>
2024-11-09 22:03:19 +0545

    chore: Refactor MongoDB connection code for cleaner formatting

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-    try{
-        const connectionInstance = await mongoose.connect
-        (`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
-        console.log(`MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`)
-    }catch(error){
-        console.log("MongoDB Connection Error",error);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(
+      `MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB Connection Error", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
